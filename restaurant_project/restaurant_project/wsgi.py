9da615fc653619400f89fbc1e276bc778af8eb5a
Muhammad Usama <123225197+usamayo1@users.noreply.github.com>
2026-06-19 18:57:52 +0500

    Update wsgi.py
    change the code with path

--- restaurant_project/restaurant_project/wsgi.py
+++ restaurant_project/restaurant_project/wsgi.py
@@ -6,11 +6,19 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+path = "/home/usama60/Restaurant-Deploy-PythonAnywhere/restaurant_project"
+
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "restaurant_project.settings"
+)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_project.settings')
 
 application = get_wsgi_application()
