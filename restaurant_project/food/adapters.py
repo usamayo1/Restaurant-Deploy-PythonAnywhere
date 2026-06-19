95951261e6917ba2f4140c74d1b072f62b4212e2
Muhammad Usama <123225197+usamayo1@users.noreply.github.com>
2026-06-19 23:24:24 +0500

    Update adapters.py
    replace the code of adapters.py

--- restaurant_project/food/adapters.py
+++ restaurant_project/food/adapters.py
@@ -1,32 +1,33 @@
-# from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
-# class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-
-#     def save_user(self, request, sociallogin, form=None):
-#         user = super().save_user(request, sociallogin, form)
-
-#         picture_url = sociallogin.account.extra_data.get('picture')
-
-#         if picture_url:
-#             user.userprofile.profile_image = picture_url
-#             user.userprofile.save()
-
-#         return user
-    
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from .models import UserProfile
+
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
 
-        picture_url = sociallogin.account.extra_data.get("picture")
+        data = sociallogin.account.extra_data
 
+        # Save Google email
+        user.email = data.get("email", user.email)
+
+        # Save first name
+        user.first_name = data.get("given_name", user.first_name)
+
+        # Save last name
+        user.last_name = data.get("family_name", user.last_name)
+
+        user.save()
+
+        # Create profile if it doesn't exist
         profile, created = UserProfile.objects.get_or_create(user=user)
 
+        # Save profile picture
+        picture_url = data.get("picture")
         if picture_url:
             profile.profile_image = picture_url
-            profile.save()
+
+        profile.save()
 
         return user
